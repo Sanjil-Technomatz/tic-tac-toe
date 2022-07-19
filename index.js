046916ac580d42783a94034e12a6b6b673c0f7b1
sanjil Kaushal <skaushal@technomatz.com>
2022-07-19 12:20:01 +0530

    final commit

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 flag = 1;
 
-function check() {
+ var p1 = localStorage.getItem("user_1");
+ var p2 = localStorage.getItem("user_2");
 
+ window.onload = function(){
+	document.getElementById("p1").innerHTML=p1;
+	document.getElementById("p2").innerHTML=p2;
+ }
+
+function check() {
 	var k1, k2, k3, k4, k5, k6, k7, k8, k9;
 	k1 = document.getElementById("k1").value;
 	k2 = document.getElementById("k2").value;
@@ -15,18 +22,18 @@ function check() {
 
 	if (k1 == 'X' && k2 == 'X' &&  k3 == 'X') 
 	{
-		document.getElementById('print').innerHTML = "Player X won";
+		document.getElementById('print').innerHTML = p1+" won";
 		document.getElementById("k4").disabled = true;
 		document.getElementById("k5").disabled = true;
 		document.getElementById("k6").disabled = true;
 		document.getElementById("k7").disabled = true;
 		document.getElementById("k8").disabled = true;
 		document.getElementById("k9").disabled = true;
-		window.alert('Player X won');
+		window.alert(p1+' won');
 	}
 	else if (k1 == 'X' && k4 == 'X' && k7 == 'X') 
 	{
-		document.getElementById('print').innerHTML = "Player X won";
+		document.getElementById('print').innerHTML = p1+" won";
 		document.getElementById("k2").disabled = true;
 		document.getElementById("k3").disabled = true;
 		document.getElementById("k5").disabled = true;
@@ -34,162 +41,162 @@ function check() {
 		document.getElementById("k8").disabled = true;
 		document.getElementById("k9").disabled = true;
 
-		window.alert('Player X won');
+		window.alert(p1+" won");
 	}
 	else if (k7 == 'X' && k8 == 'X' &&  k9 == 'X')
 	{
-		document.getElementById('print').innerHTML = "Player X won";
+		document.getElementById('print').innerHTML = p1+" won";
 		document.getElementById("k1").disabled = true;
 		document.getElementById("k2").disabled = true;
 		document.getElementById("k3").disabled = true;
 		document.getElementById("k4").disabled = true;
 		document.getElementById("k5").disabled = true;
 		document.getElementById("k6").disabled = true;
-		window.alert('Player X won');
+		window.alert(p1+" won");
 	}
 	else if (k3 == 'X'  && k6 == 'X' &&  k9 == 'X') 
 	{
-		document.getElementById('print').innerHTML = "Player X won";
+		document.getElementById('print').innerHTML = p1+" won";
 		document.getElementById("k1").disabled = true;
 		document.getElementById("k2").disabled = true;
 		document.getElementById("k4").disabled = true;
 		document.getElementById("k5").disabled = true;
 		document.getElementById("k7").disabled = true;
 		document.getElementById("k8").disabled = true;
-		window.alert('Player X won');
+		window.alert(p1+" won");
 	}
 	else if ( k1 == 'X'&& k5 == 'X' &&k9 == 'X') 
 	{
-		document.getElementById('print').innerHTML = "Player X won";
+		document.getElementById('print').innerHTML = p1+" won";
 		document.getElementById("k2").disabled = true;
 		document.getElementById("k3").disabled = true;
 		document.getElementById("k4").disabled = true;
 		document.getElementById("k6").disabled = true;
 		document.getElementById("k7").disabled = true;
 		document.getElementById("k8").disabled = true;
-		window.alert('Player X won');
+		window.alert(p1+" won");
 	}
 	else if (k3 == 'X' && k5 == 'X' && k7 == 'X') 
 	{
-		document.getElementById('print').innerHTML = "Player X won";
+		document.getElementById('print').innerHTML = p1+" won";
 		document.getElementById("k1").disabled = true;
 		document.getElementById("k2").disabled = true;
 		document.getElementById("k4").disabled = true;
 		document.getElementById("k6").disabled = true;
 		document.getElementById("k8").disabled = true;
 		document.getElementById("k9").disabled = true;
-		window.alert('Player X won');
+		window.alert(p1+" won");
 	}
 	else if (k2 == 'X' && k5 == 'X' && k8 == 'X') 
 	{
-		document.getElementById('print').innerHTML = "Player X won";
+		document.getElementById('print').innerHTML = p1+" won";
 		document.getElementById("k1").disabled = true;
 		document.getElementById("k3").disabled = true;
 		document.getElementById("k4").disabled = true;
 		document.getElementById("k6").disabled = true;
 		document.getElementById("k7").disabled = true;
 		document.getElementById("k9").disabled = true;
-		window.alert('Player X won');
+		window.alert(p1+" won");
 	}
 	else if ( k4 == 'X' && 	k5 == 'X' && k6 == 'X') 
 	{
-		document.getElementById('print').innerHTML = "Player X won";
+		document.getElementById('print').innerHTML = p1+" won";
 		document.getElementById("k1").disabled = true;
 		document.getElementById("k2").disabled = true;
 		document.getElementById("k3").disabled = true;
 		document.getElementById("k7").disabled = true;
 		document.getElementById("k8").disabled = true;
 		document.getElementById("k9").disabled = true;
-		window.alert('Player X won');
+		window.alert(p1+" won");
 	}
 
 	else if (k1 == '0' && k2 == '0' &&  k3 == '0')
 	{
-		document.getElementById('print').innerHTML = "Player 0 won";
+		document.getElementById('print').innerHTML = p2+" won";
 		document.getElementById("k4").disabled = true;
 		document.getElementById("k5").disabled = true;
 		document.getElementById("k6").disabled = true;
 		document.getElementById("k7").disabled = true;
 		document.getElementById("k8").disabled = true;
 		document.getElementById("k9").disabled = true;
-		window.alert('Player 0 won');
+		window.alert(p2+" won");
 	}
 	else if ( k1 == '0' && 	k4 == '0' &&  k7 == '0') 
 	{
-		document.getElementById('print').innerHTML = "Player 0 won";
+		document.getElementById('print').innerHTML = p2+" won";
 		document.getElementById("k2").disabled = true;
 		document.getElementById("k3").disabled = true;
 		document.getElementById("k5").disabled = true;
 		document.getElementById("k6").disabled = true;
 		document.getElementById("k8").disabled = true;
 		document.getElementById("k9").disabled = true;
-		window.alert('Player 0 won');
+		window.alert(p2+" won");
 	}
 	else if (k7 == '0' && k8 == '0' && k9 == '0') 
 	{
-		document.getElementById('print').innerHTML = "Player 0 won";
+		document.getElementById('print').innerHTML = p2+" won";
 		document.getElementById("k1").disabled = true;
 		document.getElementById("k2").disabled = true;
 		document.getElementById("k3").disabled = true;
 		document.getElementById("k4").disabled = true;
 		document.getElementById("k5").disabled = true;
 		document.getElementById("k6").disabled = true;
-		window.alert('Player 0 won');
+		window.alert(p2+" won");
 	}
 	else if (k3 == '0'&& k6 == '0'  && k9 == '0') 
 	{
-		document.getElementById('print').innerHTML = "Player 0 won";
+		document.getElementById('print').innerHTML = p2+" won";
 		document.getElementById("k1").disabled = true;
 		document.getElementById("k2").disabled = true;
 		document.getElementById("k4").disabled = true;
 		document.getElementById("k5").disabled = true;
 		document.getElementById("k7").disabled = true;
 		document.getElementById("k8").disabled = true;
-		window.alert('Player 0 won');
+		window.alert(p2+" won");
 	}
 	else if (k1 == '0'  && 	k5 == '0' && k9 == '0') 
 	{
-		document.getElementById('print').innerHTML = "Player 0 won";
+		document.getElementById('print').innerHTML = p2+" won";
 		document.getElementById("k2").disabled = true;
 		document.getElementById("k3").disabled = true;
 		document.getElementById("k4").disabled = true;
 		document.getElementById("k6").disabled = true;
 		document.getElementById("k7").disabled = true;
 		document.getElementById("k8").disabled = true;
-		window.alert('Player 0 won');
+		window.alert(p2+" won");
 	}
 	else if (k3 == '0'  && k5 == '0' && k7 == '0' ) 
 	{
-		document.getElementById('print').innerHTML = "Player 0 won";
+		document.getElementById('print').innerHTML = p2+" won";
 		document.getElementById("k1").disabled = true;
 		document.getElementById("k2").disabled = true;
 		document.getElementById("k4").disabled = true;
 		document.getElementById("k6").disabled = true;
 		document.getElementById("k8").disabled = true;
 		document.getElementById("k9").disabled = true;
-		window.alert('Player 0 won');
+		window.alert(p2+" won");
 	}
 	else if (k2 == '0'  && 	k5 == '0' && k8 == '0') 
 	{
-		document.getElementById('print').innerHTML = "Player 0 won";
+		document.getElementById('print').innerHTML = p2+" won";
 		document.getElementById("k1").disabled = true;
 		document.getElementById("k3").disabled = true;
 		document.getElementById("k4").disabled = true;
 		document.getElementById("k6").disabled = true;
 		document.getElementById("k7").disabled = true;
 		document.getElementById("k9").disabled = true;
-		window.alert('Player 0 won');
+		window.alert(p2+" won");
 	}
 	else if (k4 == '0' && k5 == '0' && k6 == '0')
 	{
-		document.getElementById('print').innerHTML = "Player 0 won";
+		document.getElementById('print').innerHTML = p2+" won";
 		document.getElementById("k1").disabled = true;
 		document.getElementById("k2").disabled = true;
 		document.getElementById("k3").disabled = true;
 		document.getElementById("k7").disabled = true;
 		document.getElementById("k8").disabled = true;
 		document.getElementById("k9").disabled = true;
-		window.alert('Player 0 won');
+		window.alert(p2+" won");
 	}
 
 	else if ((k1 == 'X' || k1 == '0') && (k2 == 'X' || k2 == '0') && (k3 == 'X' || k3 == '0') && (k4 == 'X' || k4 == '0') &&
@@ -202,11 +209,10 @@ function check() {
 			else 
 			{
 				if (flag == 1) {
-				document.getElementById('print').innerHTML = "Player X Turn";
+				document.getElementById('print').innerHTML = p1+"'s turn";
 		}
 		else {
-			document.getElementById('print')
-				.innerHTML = "Player 0 Turn";
+			document.getElementById('print').innerHTML = p2+"'s turn";
 		}
 	}
 }
--- main.js
+++ main.js
@@ -37,9 +37,34 @@ function saveData(){
     {
         let current_user=user_records.filter((v)=>{return v.email==email && v.pass==pass})[0]
 
-        localStorage.setItem('current_user',current_user.name);
+        localStorage.setItem('user_1',current_user.name);
         alert('Login success as ' + current_user.email +" Email Id");  
         event.preventDefault();
+        window.location.href="login2.html";
+    }
+    else
+    {
+        alert('Login Fail - Wrong email or password');
+    }
+ }
+ function login2()
+ {
+    
+    let email,pass;
+    email=document.getElementById("email").value;
+ 
+    pass=document.getElementById("pass").value;
+ 
+    let user_records=new Array();
+    user_records=JSON.parse(localStorage.getItem("user"))?JSON.parse(localStorage.getItem("user")):[]
+    if(user_records.some((v)=>{return v.email==email && v.pass==pass}))
+    {
+        let current_user=user_records.filter((v)=>{return v.email==email && v.pass==pass})[0]
+
+        localStorage.setItem('user_2',current_user.name);
+        alert('Login success as ' + current_user.email +" Email Id");  
+        event.preventDefault();
+        window.location.href="dashboard.html";
     }
     else
     {
